46c2761973ba9f6cd30e9830300c7d4483ab3a42
bellash13 <theobellash@gmail.com>
2024-06-10 18:14:54 +0200

    Citizen register

--- app.py
+++ app.py
@@ -1,3 +1,28 @@
+citizens = []
+leave = False
+totalMale = 0
+totalFemale = 0
 
-name = input("Enter your name: ")
-print(f'Hello {name}!')
+while True and not leave:
+    name = input("Enter citizen name: ")
+
+    gender = input("Is citizen male (M) or female(F): ")
+    if gender == 'M':
+        totalMale += 1
+    elif gender == 'F':
+        totalFemale += 1
+    else:
+        print('Invalide gender. Use F for female or M for male')
+        continue
+
+    age = int(input("How old is the citizen: "))
+
+    citizen = {name:name, gender:gender, age: age}
+
+    citizens.append(citizen)
+    
+    choice = input("Stop adding citizen? Yes(Y) or No (N)")
+    
+    leave = choice == 'Y'
+
+print(f"Total male: {totalMale} total female {totalFemale}")
